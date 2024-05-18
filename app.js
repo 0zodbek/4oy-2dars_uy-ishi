a84ba22cc0bd01f9c0bbfc082b6dad4c02fbb727
0zodbek <ozpdbekov@gmail.com>
2024-05-19 01:11:58 +0500

    issues from js

---
+++ app.js
@@ -0,0 +1,179 @@
+// 1-masala
+
+// alert("n butun son kiriting , berilgan son agar musbat bolsa 1 ga oshiriladi aks holda natijani ekranga chiqariladi");
+
+// let n = +prompt('n sonni kiriting');
+
+// if (n > 0) {
+
+//     console.log( ++n );
+
+// } else {
+
+//     console.log(n);
+
+// };
+
+// 2-masala
+
+// alert("m butun son kiriting , berilgan son agar musbat bolsa 1 ga oshiriladi aks holda ikkiga kamaytiriladi natijani ekranga chiqariladi");
+
+// let m = +prompt('m sonni kiriting');
+
+// if ( m > 0 ) {
+
+//     console.log( ++m );} else{
+
+//         console.log(-2+m); 
+
+//     };
+
+
+// 3-masala
+
+// alert("k butun son kiriting , berilgan son agar musbat bolsa 1 ga oshiriladi aks holda ikkiga kamaytiriladi 0 ga teng bo'lsa 10 ni o'zlashtirsin natijani ekranga chiqariladi");
+
+// let k = +prompt("k sonni kiriting");
+
+// if ( k > 0 ) {
+
+// console.log( ++k );  } else{
+
+// if ( k == 0 ) {
+
+// console.log(10); } else{
+
+// console.log( -2+k ); }};
+
+// 4-masala
+
+// alert('uchta son kiriting musbatlarini aniqlovchi algoritm');
+
+// let A = +prompt('A');
+
+// let B = +prompt('B');
+
+// let C = +prompt('C');
+
+// if (A > 0) {
+
+//     A = 1 } else{
+
+//     A = 0 };
+
+// if (B > 0) {
+
+//     B = 1 } else{
+    
+//     B = 0 };
+
+// if (C > 0) {
+
+//     C = 1 } else{
+        
+//     C = 0 };
+
+//     console.log(A+B+C);
+
+// 5-masala
+
+// alert('uchta son kiriting nechtasi musbat nechtasi manfiy aniqlovchi algoritm');
+
+// let M = +prompt ('M ni kiriting');
+
+// let N = +prompt ('N ni kiriting');
+
+// let K = +prompt ('K ni kiriting');
+
+// let musbat = 0;
+
+// let manfiy = 0;
+
+// if (M > 0){
+
+// ++musbat} else {
+
+// ++manfiy};
+
+// if (N > 0){
+
+// ++musbat}
+
+// else {
+
+// ++manfiy};
+
+// if (K > 0){
+
+// ++musbat}
+    
+// else {
+
+// ++manfiy};
+
+// console.log('musbat'), console.log(musbat);
+
+// console.log("manfiy"), console.log(manfiy);
+
+// 6-masala
+
+// alert('ikkita buun son kiriting kattasini aniqlovchi algoritm');
+
+// let J = +prompt('J');
+
+// let H = +prompt('H');
+
+// if (J > H){
+
+// console.log(J);}else{
+
+// console.log(H);}
+
+// 7-masala
+
+// alert('ikkita buun son kiriting kichigini tartib raqamini aniqlovchi algoritm');
+
+let x = +prompt('x')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 13-masala
+
+// let A = +prompt('A');
+
+// let B = +prompt('B');
+
+// let C = +prompt('C');
+
+// let max ;
+
+// let norm ;
+
+// let min ;
+
+// if (A > B){
+// max = A,min = B} else {
+
+// max = B,min = A} if(max < C){
+
+// norm = max} else if (C < min){
+
+// norm = min} else{
+
+// norm = C}
+
+// console.log(norm);
+
+
